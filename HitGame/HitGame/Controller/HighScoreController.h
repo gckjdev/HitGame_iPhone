92b97596073e6907f1adae501f4ce844e9033a4f
kira <ouyongyong@163.com>
2012-01-07 17:07:17 +0800

    re make high score

--- HitGame/HitGame/Controller/HighScoreController.h
+++ HitGame/HitGame/Controller/HighScoreController.h
@@ -2,12 +2,13 @@
 //  HighScoreController.h
 //  HitGame
 //
-//  Created by Orange on 12-1-6.
+//  Created by Orange on 12-1-7.
 //  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
 //
 
-#import "HGTableViewController.h"
+#import <UIKit/UIKit.h>
 
-@interface HighScoreController : HGTableViewController
+@interface HighScoreController : UIViewController <UITableViewDelegate, UITableViewDataSource>
+@property (retain, nonatomic) IBOutlet UITableView *dataTableView;
 
 @end
--- HitGame/HitGame/Controller/HighScoreController.m
+++ HitGame/HitGame/Controller/HighScoreController.m
@@ -2,13 +2,14 @@
 //  HighScoreController.m
 //  HitGame
 //
-//  Created by Orange on 12-1-6.
+//  Created by Orange on 12-1-7.
 //  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
 //
 
 #import "HighScoreController.h"
 
 @implementation HighScoreController
+@synthesize dataTableView;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -37,6 +38,7 @@
 
 - (void)viewDidUnload
 {
+    [self setDataTableView:nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
     // e.g. self.myOutlet = nil;
@@ -46,6 +48,22 @@
 {
     // Return YES for supported orientations
     return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+- (void)dealloc {
+    [dataTableView release];
+    [super dealloc];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell* cell = [[UITableViewCell alloc] init ];
+    return cell;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 66;
 }
 
 @end
